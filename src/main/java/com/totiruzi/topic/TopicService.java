02fd0a69e307fc3eb99415855b0fe252f6b197d6
Totiruzi <totiruzi@yahoo.com>
2017-11-09 08:11:30 +0300

    Spring Boot RestfulAPI

---
+++ src/main/java/com/totiruzi/topic/TopicService.java
@@ -0,0 +1,48 @@
+package com.totiruzi.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("angular4", "Angular4", "Angular4 Description")
+			
+			));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		//filtering through the topics to get a topic
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+
+}
